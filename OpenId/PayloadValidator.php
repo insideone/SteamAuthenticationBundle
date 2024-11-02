6be5234caae989b7c1aeb65bd6c81b323d830d51
inside <xim.nenko@gmail.com>
2024-11-02 22:45:41 +0300

    feat: align openid login parameters with recent changes from steam

--- OpenId/PayloadValidator.php
+++ OpenId/PayloadValidator.php
@@ -24,6 +24,16 @@ class PayloadValidator
             'mode' => 'check_authentication',
         ]);
 
+        # Steam has changed something.
+        # https://github.com/knojector/SteamAuthenticationBundle/pull/13
+        foreach ($params as $paramKey => $paramValue) {
+            if (strpos($paramKey, 'openid_') === 0) {
+                $newKey = str_replace('openid_', 'openid.', $paramKey);
+                unset($params[$paramKey]);
+                $params[$newKey] = $paramValue;
+            }
+        }
+
         $response = $this->client->post(self::URL, [
             'form_params' => $params
         ])->getBody()->getContents();
